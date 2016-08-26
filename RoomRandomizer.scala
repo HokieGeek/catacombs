e865a15e86bc683eb8128b095cc2dd2b9eb7863b
HokieGeek <andres.f.perez@gmail.com>
2016-08-26 01:41:15 -0400

    Baby steps

--- RoomRandomizer.scala
+++ RoomRandomizer.scala
@@ -1,9 +1,43 @@
 import scala.io.Source
 
+object Sets extends Enumeration {
+  type Sets = Value
+  val Base = Value("Base")
+}
+import Sets._
+
+class Board(_id: Integer, _set: Sets, _name: String, _numObstacles: Integer) {
+  def id = _id
+  def set = _set
+  def name = _name
+  def numObstacles = _numObstacles
+
+  override def toString() =
+    "[" + id + "] " + name + " / " + numObstacles + " (" + set + ")"
+}
+
+/*
+class Lord() {
+}
+
+class Monster() {
+}
+
+class Room() {
+}
+*/
+
 object RoomRandomizer {
-  def main(args: Array[String]) {
-    val src = Source.fromFile("./data/rooms.csv")
+  def getBoards = {
+    val src = Source.fromFile("./data/boards.csv")
     val iter = src.getLines().drop(1).map(_.split(","))
-    iter foreach(a => println(a(0)))
+    iter foreach(a => println(a(0)+" "+a(2)))
+    // TODO: figure out if reading header can be used to figure out the column needed
+    // var Boards:Map[Integer, Board] = Map()
+    // iter foreach(a => Boards + = (a(0) -> new Board(a(0), a(1), a(2), a(3))))
+  }
+
+  def main(args: Array[String]) {
+    getBoards
   }
 }
