11364834ae479c85142f448f54e1429e53a1370c
HokieGeek <andres.f.perez@gmail.com>
2016-08-27 21:53:48 -0400

    Ok, that is some progress

--- Collection.scala
+++ Collection.scala
@@ -1,40 +1,72 @@
+import scala.io.Source
+
 object Sets extends Enumeration {
   type Sets = Value
   val Base = Value("Base")
+  val Soloth = Value("Soloth")
+  val Vermin = Value("Vermin")
 }
 import Sets._
 
-class Board(_name: String, _set: Sets, _numObstacles: Int) {
-  def name = _name
-  def set = _set
-  def numObstacles = _numObstacles
+// class Thing2[T] {
+//   def create(implicit m: ClassManifest[T]) = m.erasure.create
+// }
+
+class Thing { }
+
+class Board(entry: Array[String]) extends Thing {
+  def set = Sets.withName(entry(1))
+  def name = entry(2)
+  def numObstacles = entry(3)
 
   override def toString() = name + " / " + numObstacles + " (" + set + ")"
 }
 
-/*
-class Lord() {
+class Monster(entry: Array[String]) extends Thing {
+  // ID   Set   Name   Class   Health   Reward   Special Ability   Actions
+  def set = Sets.withName(entry(1))
+  def name = entry(2)
+  def charClass = entry(3)
+  def health = entry(4)
+  def reward = entry(5)
+  def special = entry(6)
+  def actions = entry(7)
+
+  override def toString() = name
 }
 
-class Monster() {
+/*
+class Lord() {
+  // Name   Set   Health   Board   Wanderer   Special Action   Minions
 }
 
 class Room() {
+  // Name   Set   Type   Level   Board   N   E   S   W Monsters[]
 }
 */
 
 object Collection {
-  // def getBoards : collection.mutable.Map[Int, Board] = {
-  def getBoards : Map[Int, Board] = {
-    val src = scala.io.Source.fromFile("./data/boards.csv")
+
+  def getBoards(file: String) : Map[Int, Board] = {
+    val src = Source.fromFile(file)
     val iter = src.getLines().drop(1).map(_.split(","))
-    // TODO: figure out if reading header can be used to figure out the column needed
 
-    var boards = Map[Int, Board]()
-    iter foreach(a => boards = boards + (a(0).toInt -> new Board(_name = a(2), _set = Base, _numObstacles = a(0).toInt)))
+    var things = Map[Int, Board]()
+    iter foreach(a => things = things + (a(0).toInt -> new Board(a)))
 
-    return boards
+    return things
   }
 
-  val Boards = getBoards
+  def getMonsters(file: String) : Map[Int, Monster] = {
+    val src = Source.fromFile(file)
+    val iter = src.getLines().drop(1).map(_.split(","))
+
+    var things = Map[Int, Monster]()
+    iter foreach(a => things = things + (a(0).toInt -> new Monster(a)))
+
+    return things
+  }
+
+  val Boards = getBoards("./data/boards.csv")
+  val Monsters = getMonsters("./data/monsters.csv")
 }
--- RoomRandomizer.scala
+++ RoomRandomizer.scala
@@ -1,6 +1,9 @@
 object RoomRandomizer {
   def main(args: Array[String]) {
 
+    println("[BOARDS]")
     Collection.Boards.foreach(a => println(a))
+    println("[MONSTERS]")
+    Collection.Monsters.foreach(a => println(a))
   }
 }
